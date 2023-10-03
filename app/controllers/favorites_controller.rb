1bee894f262826f71ae2048228f40372b7a2daf3
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-03 13:33:23 +0000

    いいね実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,18 +1,17 @@
 class FavoritesController < ApplicationController
 
-  def create
+  def create #いいねする
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to books_path(book)
+    redirect_to request.referer #同じページをリダイレクト
   end
 
-  def destroy
-    book = Book.find(params[:post_image_id])
+  def destroy #いいね消す
+    book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to books_path(book)
-
+    redirect_to request.referer #同じページをリダイレクト
   end
 
 end
