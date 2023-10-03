634342f1e61476489bc75c3c1789775e82923cb9
EC2 Default User <143916229+daisuke8331@users.noreply.github.com>
2023-10-03 16:26:39 +0000

    空のコメント投稿時にエラー

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,10 +1,19 @@
 class BookCommentsController < ApplicationController
 
-  def create #コメントする
+  def create #コメントを投稿する
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
+    if comment.save
+      redirect_to request.referer
+    else
+      @user = profile_user.find(params[:id])#ここでnameerror
+      render "books/show"
+    end
+  end
+
+  def destroy #コメントを削除する
+    BookComment.find(params[:id]).destroy
     redirect_to request.referer
   end
 
--- app/models/book_comment.rb
+++ app/models/book_comment.rb
@@ -3,4 +3,6 @@ class BookComment < ApplicationRecord
   belongs_to :user
   belongs_to :book
 
+  validates :comment, presence: true #空欄のままコメントを投稿できない設定s
+
 end
